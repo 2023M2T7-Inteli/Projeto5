26af91baa86ffcb5ce3176a59c35eb34bfd0d2d2
mauroDasChagas <mauro.junior@sou.inteli.edu.br>
2023-06-03 20:02:16 -0300

    Updating dynamic creation of fields

--- Codigo/app1.0/public/js/c_protocol.js
+++ Codigo/app1.0/public/js/c_protocol.js
@@ -6,6 +6,7 @@
     // Variáveis de contagem;
     let sampleCount = 0;
     let stepCount = 0;
+    let fieldCount = 0;
 
     // Variáveis de elementos criados dinâmicamente;
     let inputNameSample;
@@ -195,7 +196,7 @@ function createNewStep() {
 
         const createNewFieldButton = document.createElement("button");
         createNewFieldButton.type = "button";
-        // createNewFieldButton.onclick = createNewField;
+        createNewFieldButton.onclick = createNewField;
         createNewFieldButton.textContent = "Add field";
     //
 
@@ -214,4 +215,57 @@ function createNewStep() {
 
     // running
     stepsContainer.appendChild(divStepContainer); // ...
+};
+
+function createNewField() {
+    fieldCount++;
+
+    const fieldsContainer = this.closest(".step-container").querySelector(".fields-container");
+
+    const divFieldContainer = document.createElement("div");
+    divFieldContainer.setAttribute("data-name", `step${stepCount}_field${fieldCount}`);
+    divFieldContainer.classList.add("field-container");
+
+    const fieldFieldSet = document.createElement("fieldset");
+    fieldFieldSet.setAttribute('id', 'stepFieldset');
+    fieldFieldSet.style.border = '1px solid black';
+
+    //Creating stepFieldSet elements
+        const fieldLegend = document.createElement('legend');
+        fieldLegend.textContent = `Field ${fieldCount}: `;
+
+        const fieldInputData = document.createElement("input");
+        fieldInputData.type = "text";
+        fieldInputData.placeholder = "Enter the desired data";
+        divFieldContainer.appendChild(fieldInputData);
+
+        const fieldInputType = document.createElement("select");
+
+        //Creating fieldInputType options
+            const option1 = document.createElement("option");
+            option1.value = "image";
+            option1.text = "Image";
+
+            const option2 = document.createElement("option");
+            option2.value = "text";
+            option2.text = "Text";
+
+            const option3 = document.createElement("option");
+            option3.value = "number";
+            option3.text = "Number";
+        //
+    //
+
+    fieldInputType.appendChild(option1);
+    fieldInputType.appendChild(option2);
+    fieldInputType.appendChild(option3);
+
+    fieldFieldSet.appendChild(fieldLegend);
+    fieldFieldSet.appendChild(fieldInputData);
+    fieldFieldSet.appendChild(fieldInputType);
+
+    divFieldContainer.appendChild(fieldFieldSet);
+    
+    // running
+    fieldsContainer.appendChild(divFieldContainer);
 };
