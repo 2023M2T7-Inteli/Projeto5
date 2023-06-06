25270c11bfed527de73b8043f1f57c3891313fa8
mauroDasChagas <mauro.junior@sou.inteli.edu.br>
2023-06-05 22:11:16 -0300

    Updating login encryption system
    - I will modularize this later and apply it when sending responses to the database

--- Codigo/app1.0/src/controllers/loginRegisterController.js
+++ Codigo/app1.0/src/controllers/loginRegisterController.js
@@ -1,16 +1,37 @@
 // Importing node modules;
 const db = require('../../db');
 const path = require('path');
+const crypto = require("crypto-js");
+
+// Data cryptography
+
+// Windows: set ENCRYPTION_KEY=MinhaChaveDeCriptografia
+// const encryptionKey = process.env.ENCRYPTION_KEY;
+
+const encryptionKey = "apotichEncryptionKey";
+
+function crypt(data) {
+    const encryptedData = crypto.AES.encrypt(data, encryptionKey).toString();
+    return encryptedData;
+};
+
+function decrypt(encryptedData) {
+    const decryptedData = crypto.AES.decrypt(encryptedData, encryptionKey).toString(crypto.enc.Utf8);
+    return decryptedData;
+};
+
 
 // Register
-
 function getRegisterPage(req, res) {
     res.sendFile(path.join(__dirname, '..', '..' + '/views/main/register.html'));
 }
 
 function registering(req, res) {
     const { name_user, password_user, type_user } = req.body;
-    db.run('INSERT INTO tbl_users (name_user, password_user, type_user) VALUES (?, ?, ?)', [name_user, password_user, type_user], (err) => {
+    const encryptedName = crypt(name_user);
+    const encryptedPassword = crypt(password_user);
+    
+    db.run('INSERT INTO tbl_users (name_user, password_user, type_user) VALUES (?, ?, ?)', [encryptedName, encryptedPassword, type_user], (err) => {
         if (err) {
             console.error(err);
             res.status(500).send('Error creating user');
@@ -21,22 +42,54 @@ function registering(req, res) {
 };
 
 // Login
-
 function logging(req, res) {
     const { name_user, password_user } = req.body;
 
-    db.get('SELECT * FROM tbl_users WHERE name_user = ? AND password_user = ? ', [ name_user, password_user ], (err, row) => {
+    db.each('SELECT id_user, name_user, password_user FROM tbl_users', (err, row) => {
         if (err) {
             console.error(err.message);
-            res.status(500).send('Internal server error');
-        } else if (!row) {
-            res.status(401).send('Invalid username or password');
         } else {
-            if (row.type_user == "Coletor") {
-                res.redirect('/home_collector');
-            } else if (row.type_user == "Pesquisador") {
-                res.redirect('/home_researcher');
-            }
+            const decryptedName = decrypt(row.name_user);
+            const decryptedPassword = decrypt(row.password_user);
+
+            db.run('UPDATE tbl_users SET name_user = ?, password_user = ? WHERE id_user = ?', [decryptedName, decryptedPassword, row.id_user], (err) => {
+                if (err) {
+                    console.error(err.message);
+                } else {
+
+                    db.get('SELECT * FROM tbl_users WHERE name_user = ? AND password_user = ? ', [ name_user, password_user ], (err, row) => {
+                        if (err) {
+                            console.error(err.message);
+                            res.status(500).send('Internal server error');
+                        } else if (!row) {
+                            res.status(401).send('Invalid username or password');
+                        } else {
+                            if (row.type_user == "Coletor") {
+                                res.redirect('/home_collector');
+                            } else if (row.type_user == "Pesquisador") {
+                                res.redirect('/home_researcher');
+                            }
+
+                            db.each('SELECT id_user, name_user, password_user FROM tbl_users', (err, row) => {
+                                if (err) {
+                                    console.error(err.message);
+                                } else {
+                                    const encryptedName = crypt(name_user);
+                                    const encryptedPassword = crypt(password_user);
+
+                                    db.run('UPDATE tbl_users SET name_user = ?, password_user = ? WHERE id_user = ?', [encryptedName, encryptedPassword, row.id_user], (err) => {
+                                        if (err) {
+                                            console.error(err.message);
+                                        } else {
+                                            console.log("Data Updated");
+                                        }
+                                    });
+                                }
+                            })
+                        }
+                    });
+                }
+            });
         }
     });
 };
