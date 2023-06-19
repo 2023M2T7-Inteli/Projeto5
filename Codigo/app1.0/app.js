5f6704adf37c32991b41d1e826b3e9960b70adfb
mauroDasChagas <mauro.junior@sou.inteli.edu.br>
2023-06-19 00:13:56 -0300

    Integration using images done
    "Eu si divirto" provavelmente foi uma das imagens mais usadas para testes desse grupo

--- Codigo/app1.0/app.js
+++ Codigo/app1.0/app.js
@@ -65,6 +65,7 @@ app.get('/researcherProtocolsProgress', researcherController.getProtocolsInProgr
                         } else {
                             const modifiedHTML = data
                                 .replace('{{id_protocol}}', row.id_protocol)
+                                .replace('{{coverImage_protocol}}', row.coverImage_protocol)
                                 .replace('{{name_protocol}}', row.name_protocol)
                                 .replace('{{objective_protocol}}', row.objective_protocol);
                             res.set('Content-Type', 'text/html');
