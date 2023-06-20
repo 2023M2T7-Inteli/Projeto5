6f1f1c4a968538e932f3ca79e21c09dcf6939168
mauroDasChagas <mauro.junior@sou.inteli.edu.br>
2023-06-19 22:59:06 -0300

    answers integration

--- Codigo/app1.0/public/js/listingProtocolsFinished.js
+++ Codigo/app1.0/public/js/listingProtocolsFinished.js
@@ -38,47 +38,34 @@ function getDinamicFormsData() {
                         // $('#descricao-etapa').text(response[j].description_step);
                         // $('#id-etapa').text(response[j].id_step);
                         $.post('/read_field', { id_step: response[j].id_step }, (res) => {
-                            // console.log(res);
                             let divCampos = $('<div>');
                             fieldsetStep.append(divCampos);
                             for (i = 0; i < res.length; i++) {
-                                // console.log(res[i].name_field + res[i].id_field);
                                 var div = $('<div>');
                                 divCampos.append(div);
                                 var label = $('<label>');
-                                var input = $('<input>');
+                                var span = $('<span>'); // Substituição do elemento input por span
+                                let br = $('<br>'); 
                                 div.append(label);
+                                div.append(br);
+                                div.append(br);
                                 label.attr('id', res[i].name_field + res[i].id_field)
                                 label.text(res[i].name_field);
                                 if (res[i].description_field === 'image') {
-                                    let divImage = $('<div>');
-                                    divImage.attr('class', 'drop-zone');
-                                    div.append(divImage);
-                                    let buttonImage = $('<button>');
-                                    buttonImage.attr('class', 'btn');
-                                    let spanImage = $('<span>');
-                                    spanImage.attr('class', 'drop-zone__prompt');
-                                    spanImage.text("Clique aqui");
-                                    divImage.append(buttonImage, spanImage, input);
-                                    input.attr('type', 'file');
-                                    input.attr('accept', 'image/*');
-                                    input.attr('class', 'drop-zone__input');
-                                    input.attr('name', 'myFile');
-                                    // divImage.click(() => {
-                                    //   console.log('aiaiaii!');
-                                    // })
-                                    AddDropImage()
+                                    div.append(img); // Substituição do elemento input por img
+                                    img.addClass('input-el');
+                                    img.src(res[i].answer_field); // Deixe o span vazio para exibir a informação do número
                                 } else if (res[i].description_field === 'number') {
-                                    div.append(input);
-                                    input.attr('type', 'number');
-                                    input.addClass('input-el');
+                                    div.append(span); // Substituição do elemento input por span
+                                    span.addClass('input-el');
+                                    span.text(res[i].answer_field); // Deixe o span vazio para exibir a informação do número
                                 } else {
-                                    div.append(input);
-                                    input.attr('type', 'text');
-                                    input.addClass('input-el');
+                                    div.append(span); // Substituição do elemento input por span
+                                    span.addClass('input-el');
+                                    span.text(res[i].answer_field); // Deixe o span vazio para exibir a informação do texto
                                 }
-                                input.attr('id', res[i].id_field);
-                            };
+                                span.attr('id', res[i].id_field);
+                            }
                         }, 'json');
                     };
                 }, 'json');
@@ -88,61 +75,4 @@ function getDinamicFormsData() {
 };
 function typeImage(input) {
     input.attr('type', 'file');
-};
-
-// function that add the listeners to the dropbox
-function AddDropImage() {
-    document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
-        const dropZoneElement = inputElement.closest(".drop-zone");
-        dropZoneElement.addEventListener("click", (e) => {
-            inputElement.click();
-        });
-        inputElement.addEventListener("change", (e) => {
-            if (inputElement.files.length) {
-                updateThumbnail(dropZoneElement, inputElement.files[0]);
-            }
-        });
-        dropZoneElement.addEventListener("dragover", (e) => {
-            e.preventDefault();
-            dropZoneElement.classList.add("drop-zone--over");
-        });
-        ["dragleave", "dragend"].forEach((type) => {
-            dropZoneElement.addEventListener(type, (e) => {
-                dropZoneElement.classList.remove("drop-zone--over");
-            });
-        });
-        dropZoneElement.addEventListener("drop", (e) => {
-            e.preventDefault();
-            if (e.dataTransfer.files.length) {
-                inputElement.files = e.dataTransfer.files;
-                updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-            }
-            dropZoneElement.classList.remove("drop-zone--over");
-        });
-    });
-}
-
-// Update the drop image to the image that was chosen
-function updateThumbnail(dropZoneElement, file) {
-    let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
-    // First time - remove the prompt
-    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
-        dropZoneElement.querySelector(".drop-zone__prompt").remove();
-    }
-    if (!thumbnailElement) {
-        thumbnailElement = document.createElement("div");
-        thumbnailElement.classList.add("drop-zone__thumb");
-        dropZoneElement.appendChild(thumbnailElement);
-    }
-    thumbnailElement.dataset.label = file.name;
-    // Show thumbnail for image files
-    if (file.type.startsWith("image/")) {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-            thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
-        };
-    } else {
-        thumbnailElement.style.backgroundImage = null;
-    }
 };
